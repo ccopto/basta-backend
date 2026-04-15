a68dfd7ce0caccbd5b4abf62ecd7b475d6cdf442
Carlos Copto <carlos.copto@colibrigroup.com>
2026-04-15 13:08:08 -0600

    feat(dto): add expected missing UI fields to LobbySnapshot

--- Basta.Server/DTOs/LobbyDTOs.cs
+++ Basta.Server/DTOs/LobbyDTOs.cs
@@ -5,6 +5,8 @@ public class LobbyPlayer
     public int UserId { get; set; }
     public string Nickname { get; set; } = string.Empty;
     public int Score { get; set; } // Will be used in future stories for cumulative score
+    public bool IsHost { get; set; }
+    public bool IsOnline { get; set; } = true; // always true in this phase
 }
 
 public class LobbySnapshot
@@ -13,5 +15,7 @@ public class LobbySnapshot
     public int TotalRounds { get; set; }
     public int TimerDuration { get; set; }
     public int HostUserId { get; set; }
+    public string Language { get; set; } = string.Empty;
+    public string State { get; set; } = "Lobby";
     public IEnumerable<LobbyPlayer> Players { get; set; } = Enumerable.Empty<LobbyPlayer>();
 }
