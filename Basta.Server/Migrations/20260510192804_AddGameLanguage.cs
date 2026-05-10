c166537716fc55df07e6b12871fc990a4d5fe008
Carlos Copto <carlos.copto@colibrigroup.com>
2026-05-10 14:35:02 -0600

    feat(migrations): add AddGameLanguage, AddCategoryValidationType, AddRoundAnswerDictionaryValidation

---
+++ Basta.Server/Migrations/20260510192804_AddGameLanguage.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Basta.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGameLanguage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "DictionaryValid",
+                table: "RoundAnswers",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "RequiresPeerReview",
+                table: "RoundAnswers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Language",
+                table: "Games",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ValidationType",
+                table: "Categories",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 1,
+                column: "ValidationType",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 2,
+                column: "ValidationType",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 3,
+                column: "ValidationType",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 4,
+                column: "ValidationType",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 5,
+                column: "ValidationType",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 6,
+                column: "ValidationType",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 7,
+                column: "ValidationType",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 8,
+                column: "ValidationType",
+                value: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DictionaryValid",
+                table: "RoundAnswers");
+
+            migrationBuilder.DropColumn(
+                name: "RequiresPeerReview",
+                table: "RoundAnswers");
+
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "ValidationType",
+                table: "Categories");
+        }
+    }
+}
--- Basta.Server/Migrations/20260510193327_AddRoundAnswerDictionaryValidation.Designer.cs
+++ Basta.Server/Migrations/20260510193327_AddRoundAnswerDictionaryValidation.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Basta.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Basta.Server.Migrations
 {
     [DbContext(typeof(BastaDbContext))]
-    partial class BastaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260510193327_AddRoundAnswerDictionaryValidation")]
+    partial class AddRoundAnswerDictionaryValidation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.7");
